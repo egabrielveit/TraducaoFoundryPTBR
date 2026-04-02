7ee1e98b9cdf9d076a05f1f40b6168930cc471a0
egabrielveit <e.gabrielveit@gmail.com>
2026-04-02 11:17:33 +0000

    versao7 sem a pasta scripts

--- scipts/register.js
+++
@@ -1,9 +0,0 @@
-Hooks.once('init', () => {
-    if (typeof Babele !== 'undefined') {
-        Babele.get().register({
-            module: 'TraducaoFoundryPTBR',
-            lang: 'pt_BR',
-            dir: 'babele-register'
-        });
-    }
-});
